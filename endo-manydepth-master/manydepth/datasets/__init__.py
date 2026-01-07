b3160be5889ac5bc5c5ed627f133afba190cfd1b
Consuk <0228053@up.edu.mx>
2026-01-06 19:40:37 -0600

    hamlyn implementation

--- endo-manydepth-master/manydepth/datasets/__init__.py
+++ endo-manydepth-master/manydepth/datasets/__init__.py
@@ -1,3 +1,9 @@
+# flake8: noqa: F401
+from .kitti_dataset import KITTIRAWDataset, KITTIOdomDataset, KITTIDepthDataset
+from .cityscapes_preprocessed_dataset import CityscapesPreprocessedDataset
+from .cityscapes_evaldataset import CityscapesEvalDataset
+from .scared_dataset import SCAREDDataset
+from .scared_dataset import SCAREDRAWDataset
 from .kitti_dataset import KITTIRAWDataset, KITTIOdomDataset, KITTIDepthDataset
 from .scared_dataset import SCAREDDataset
 from .scared_dataset import SCAREDRAWDataset
